4c7f7c3a9f82e9ed17e5fb1c4651cd24da93490d
ozkanozdemir <ozkan.ozdemir4114@gmail.com>
2022-04-28 16:04:32 +0300

    EnemySpawner prefab yapıldı

--- Assets/Scripts/Pathfinder.cs
+++ Assets/Scripts/Pathfinder.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class Pathfinder : MonoBehaviour
 {
+    [SerializeField] private float rotateSpeed = 2f;
+    
     private WaveConfigSO _waveConfig;
     private List<Transform> _waypoints;
     private int _waypointIndex = 0;
@@ -33,16 +35,22 @@ public class Pathfinder : MonoBehaviour
     {
         if (_waypointIndex < _waypoints.Count)
         {
+            // Sonraki hedefe doğru ilerleme
             Vector3 targetPosition = _waypoints[_waypointIndex].position;
             var delta = _waveConfig.GetMoveSpeed() * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, targetPosition, delta);
             
-            Debug.Log("_waypoints.Count :" + _waypoints.Count);
-            Debug.Log("_waypointIndex :" + _waypointIndex);
+            // Sonraki hedefe doğru dönme
+            Vector3 targetDirection = targetPosition - transform.position;
+            Debug.Log("targetDirection : " + targetDirection);
+            Vector3 newDirection =
+                Vector3.RotateTowards(transform.forward, targetDirection, rotateSpeed * Time.deltaTime, 0.0f);
+            Debug.DrawRay(transform.position, newDirection, Color.red);
+            transform.rotation = Quaternion.LookRotation(newDirection);
+            
 
             if (Vector3.Distance(transform.position, targetPosition) < 0.001f)
             {
-                Debug.Log("aa");
                 _waypointIndex++;
             }
         }
