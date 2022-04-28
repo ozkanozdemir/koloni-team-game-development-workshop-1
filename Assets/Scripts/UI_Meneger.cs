a7abcb656d773dffd984cc114250bb33dd65f29f
samiyagmur <samiyagmur92@gmail.com>
2022-04-28 16:23:28 +0300

    UI And StartingUIScript

---
+++ Assets/Scripts/UI_Meneger.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Meneger : MonoBehaviour
+{
+    public Text Wave;
+    public Text Level;
+    public Text Coin;
+    public Text Exp;
+    public Text Upgrade;
+    public Text Sell;
+    public Button _Upgrade;
+    public Button _Sell;
+    public Button _CharacterZen;
+    public Button _CharacterKava;
+    public Button _CharacterCecom;
+    public Button _CharacterNazyanno;
+    public Button _TimeDelay;
+    public Button _Chaos;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
